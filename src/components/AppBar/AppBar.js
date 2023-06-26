95e274a98939eaad2d91cc8ababb6f14fb906ba8
NadiaOl <nadia.olifirova@gmail.com>
2023-06-26 16:38:07 +0300

    spiner

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,15 +1,22 @@
+import { useSelector } from 'react-redux';
 import {ContactForm} from '../ContactForm/ContactForm';
 import {ContactList} from '../ContactList/ContactList';
 import {Filter} from '../Filter/Filter'
 import css from './AppBar.module.css';
+import { selectIsLoading } from 'redux/selectors';
+import { RingLoader } from "react-spinners";
 
 export const AppBar = () => {
+      const isLoading = useSelector(selectIsLoading);
   return (
     <div className={css.phonebook}>
+
       <h2 className={css.phonebookTitle}>Phonebook</h2>
+                        
       <ContactForm />
       <Filter />
-      <h3 className={css.phonebookSubTitle}>Contacts</h3>
+      <h3 className={css.phonebookSubTitle}>{isLoading ? <RingLoader color="#b61a77"
+      size={20}/> : <p>Contacts</p>}</h3>
       <ContactList />
     </div>
   )
--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,14 +1,15 @@
 
 import { useDispatch, useSelector } from "react-redux";
 import { Contact } from "components/Contact/Contact";
-import { selectContacts, selectError, selectFilter, selectIsLoading } from "redux/selectors";
+import { selectContacts, selectError, selectFilter } from "redux/selectors";
 import { useEffect } from "react";
 import { fetchContacts } from "redux/operations";
 import css from './ContactList.module.css';
 
+
 export const ContactList = () =>{ 
     const items = useSelector(selectContacts);
-    const isLoading = useSelector(selectIsLoading);
+
     const error = useSelector(selectError);
     const filteredContactsList = useSelector(selectFilter);
     const dispatch = useDispatch();
@@ -23,9 +24,9 @@ export const ContactList = () =>{
 
     const visibleContacts = filteredContactsList ? filteredContacts : items
 
+
     return (
         <div>
-            {isLoading && <p>Loading...</p>}
             {error && <p>{error}</p>}
             {visibleContacts && <ul className={css.contactLists}>
                 {visibleContacts.map((contact) =>
